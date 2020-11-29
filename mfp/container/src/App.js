c279b16efd6719069ea48857af0d95a060a9c1d7
Eduardo Santos <e3duardo@live.com>
2020-11-29 15:53:34 -0300

    Generic Integration

--- mfp/container/src/App.js
+++ mfp/container/src/App.js
@@ -1,12 +1,7 @@
 import React from "react";
 
-import { mount as MarketingApp } from "marketing/MarketingApp";
+import MarketingApp from "./components/MarketingApp";
 
 export default () => {
-  return (
-    <div>
-      Hello
-      {/* <MarketingApp /> */}
-    </div>
-  );
+  return <MarketingApp />;
 };
