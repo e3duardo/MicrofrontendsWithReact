35722116ab6dbdc2638a00b95d4b3dc4125d8005
Eduardo Santos <e3duardo@live.com>
2020-12-01 18:42:16 -0300

    Communicating Auth Changes

--- mfp/auth/src/App.js
+++ mfp/auth/src/App.js
@@ -12,14 +12,18 @@ const generateClassname = createGenerateClassName({
   productionPrefix: "au",
 });
 
-export default ({ history }) => {
+export default ({ history, onSignIn }) => {
   return (
     <div>
       <StylesProvider generateClassName={generateClassname}>
         <Router history={history}>
           <Switch>
-            <Route path="/auth/signin" component={Signin} />
-            <Route path="/auth/signup" component={Signup} />
+            <Route path="/auth/signin">
+              <Signin onSignIn={onSignIn} />
+            </Route>
+            <Route path="/auth/signup">
+              <Signup onSignIn={onSignIn} />
+            </Route>
           </Switch>
         </Router>
       </StylesProvider>
